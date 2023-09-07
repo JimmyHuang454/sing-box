477a97bceb86bbc4cb6ebe07a8c513c281aae6fc
Jimmy Huang <jimmyhuang454@gmail.com>
2023-09-08 01:43:37 +0800

    add tag to dev-run

--- dev.bat
+++ dev.bat
@@ -1,2 +1,2 @@
-go build ./cmd/sing-box
-sing-box.exe run --config ./jls_config.json
+go build -tags "with_gvisor,with_dhcp,with_wireguard,with_utls,with_reality_server,with_clash_api,with_grpc,with_quic" ./cmd/sing-box
+:: sing-box.exe run --config ./jls_config.json
